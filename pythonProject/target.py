a12daf5702102b435a0bd4607985a6bfe1a611b4
R3ALITI3S <nfjo23@student.aau.dk>
2024-09-25 13:52:46 +0200

    Update target.py

--- pythonProject/target.py
+++ pythonProject/target.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 # Read the main image
+images_files = [
+"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg", "41.jpg", "42.jpg", "43.jpg", "44.jpg", "45.jpg", "46.jpg", "47.jpg", "48.jpg", "49.jpg", "50.jpg", "51.jpg", "52.jpg", "53.jpg", "54.jpg", "55.jpg", "56.jpg", "57.jpg", "58.jpg", "59.jpg", "60.jpg", "61.jpg", "62.jpg", "63.jpg", "64.jpg", "65.jpg", "66.jpg", "67.jpg", "68.jpg", "69.jpg", "70.jpg", "71.jpg", "72.jpg", "73.jpg", "74.jpg"
+]
 
 img_rgb = cv2.imread('Croppedandperspectivecorrectedboards/5.jpg')
 
@@ -23,10 +26,11 @@ template_files = [
 # List to store the grayscale templates
 templates = []
 
-# Read each template in grayscale and append to the list
-for template_file in template_files:
-    template = cv2.imread(template_file, cv2.IMREAD_GRAYSCALE)
-    templates.append(template)
+for images_file in images_files:
+    # Read each template in grayscale and append to the list
+    for template_file in template_files:
+        template = cv2.imread(template_file, cv2.IMREAD_GRAYSCALE)
+        templates.append(template)
 
 # best current results (30,30) and (3,3) and thresh = 7!!
 # template = cv2.resize(template, (30,30))
