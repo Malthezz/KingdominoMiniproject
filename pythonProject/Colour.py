513e78425be10eb53699809b3115116c6128b83a
R3ALITI3S <nfjo23@student.aau.dk>
2024-09-25 10:53:21 +0200

    colour

--- pythonProject/Colour.py
+++ pythonProject/Colour.py
@@ -12,3 +12,6 @@ cv2.imshow("Red windowl", red)
 cv2.imshow("green window", green)
 cv2.imshow("blue window", blue)
 cv2.waitKey(0)
+
+redred = cv2. split(input_image)
+cv2.imshow("Our window", input_image)
