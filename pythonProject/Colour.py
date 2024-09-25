5e19da8de61aa5fd5536a013ea519f8f9d6e27db
R3ALITI3S <nfjo23@student.aau.dk>
2024-09-25 10:50:34 +0200

    Crown

---
+++ pythonProject/Colour.py
@@ -0,0 +1,14 @@
+import cv2
+
+# Open picture
+input_image = cv2.imread("Croppedandperspectivecorrectedboards/12.jpg")
+red = input_image[:,:,2]
+green = input_image[:,:,1]
+blue = input_image[:,:,0]
+
+# Display the picture
+cv2.imshow("Our window", input_image)
+cv2.imshow("Red windowl", red)
+cv2.imshow("green window", green)
+cv2.imshow("blue window", blue)
+cv2.waitKey(0)
--- pythonProject/target.py
+++ pythonProject/target.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 
 # Read the main image
-img_rgb = cv2.imread('Croppedandperspectivecorrectedboards/12.jpg')
+img_rgb = cv2.imread('Croppedandperspectivecorrectedboards/1.jpg')
 
 # Convert it to grayscale
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
 # Read the template
-template = cv2.imread('Crown.png', 0)
+template = cv2.imread('Crown.png', cv2.IMREAD_GRAYSCALE)
+
+template = cv2.GaussianBlur(template, (3, 3), 10)
+
+cv2.imshow('crown_blur', template)
 
 # Store width and height of template in w and h
 w, h = template.shape[::-1]
@@ -17,7 +21,7 @@ w, h = template.shape[::-1]
 res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 
 # Specify a threshold
-threshold = 0.4
+threshold = 0.6
 
 # Store the coordinates of matched area in a numpy array
 loc = np.where(res >= threshold)
