1a27d38f19a2fd10af5ce84ea59acd9d1f4d4a85
Malthezz <malthh21@student.aau.dk>
2024-10-13 20:29:36 +0200

    NMS script forsøg 1

--- pythonProject/NMS.py
+++ pythonProject/NMS.py
@@ -1,51 +1,48 @@
-import cv2
+# import the necessary packages
 import numpy as np
-from fontTools.subset import intersect
-from networkx import intersection
-from numpy.ma.core import append
-import torch
-from target import *
-
-for pt in pt():
-    top_left = pt
-    top_right = (pt[0] + w, pt[1])  # x increases by width, y remains the same
-    bottom_left = (pt[0], pt[1] + h)  # x remains the same, y increases by height
-    bottom_right = (pt[0] + w, pt[1] + h)  # x increases by width, y increases by height
-
-    # Add rectangle coordinates to the list
-    rectangle_coords.append([top_left, top_right, bottom_left, bottom_right])
-
-def nms(
-        predictions,
-        iou_threshold,
-        prob_threshold,
-        box_format="corners"
-):
-    bboxes = rectangle_coords_np
-    assert type(bboxes) == list
-
-    bboxes = [box for box in bboxes if box[1] > prob_threshold]
-    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
-    bboxes_after_nms = []
-
-    while bboxes:
-        chosen_box = bboxes.pop(0)
-
-        bboxes = [
-            box
-            # are they of different classes?
-            for box in boxes
-            # if they are of the same class:
-            if box[0] != chosen_box[0]
-            # we compare
-            or intersection_over_union(
-                torch.tensor(chosen_box[2:]),
-                torch.tensor(chosen_box[:2]),
-                box_format = box_format,
-            )
-            < iou_threshold
-        ]
-
-        bboxes_after_nms.append(chosen_box)
-
-    return bboxes_after_nms
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+	# initialize the list of picked indexes	
+	pick = []
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+	# compute the area of the bounding boxes and sort the bounding
+	# boxes by the bottom-right y-coordinate of the bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+		# find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return boxes[pick].astype("int")
